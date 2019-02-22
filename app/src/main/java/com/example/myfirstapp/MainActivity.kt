9eb5278292b907d8a0ec51db37d59b403fc07dba
michael.carter <michael.carter@forgerock.com>
2019-02-22 10:36:46 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.myfirstapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.telephony.SmsManager
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    private val MY_PERMISSIONS_REQUEST_SEND_SMS: Int = 0
+    private var msg: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_SEND_SMS -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    sendSms(this.msg)
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    fun sendAtWorkMsg(view: View) {
+        checkPermissionAndSend("At work love.")
+    }
+
+    fun sendHomeMsg(view: View) {
+        checkPermissionAndSend("Home love.")
+    }
+
+    private fun checkPermissionAndSend(msg: String) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED) {
+
+                this.msg = msg
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    MY_PERMISSIONS_REQUEST_SEND_SMS)
+
+                // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+        } else {
+            // Permission has already been granted
+            sendSms(msg)
+        }
+    }
+
+    private fun sendSms(msg: String) {
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage("+447980134821", null, msg, null, null)
+    }
+
+
+}
